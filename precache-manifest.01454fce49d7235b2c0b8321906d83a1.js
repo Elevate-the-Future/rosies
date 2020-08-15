1bdd77a10554bb113e0c822c6aa7a68aeb371948
Alvin Zheng <vortexdomainia@gmail.com>
2020-08-14 20:50:41 -0700

    Updates

---
+++ precache-manifest.01454fce49d7235b2c0b8321906d83a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369f743f9ebe976ea2b1357cc22e86c2",
+    "url": "/rosies/index.html"
+  },
+  {
+    "revision": "ce4112d3de171e8aa477",
+    "url": "/rosies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ad91ff8b9c4248d60a1",
+    "url": "/rosies/static/js/2.911820ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rosies/static/js/2.911820ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce4112d3de171e8aa477",
+    "url": "/rosies/static/js/main.33e330ad.chunk.js"
+  },
+  {
+    "revision": "d9ed7e488f4eacedea02",
+    "url": "/rosies/static/js/runtime-main.fa807c25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rosies/static/media/logo.5d5d9eef.svg"
+  }
+]);
