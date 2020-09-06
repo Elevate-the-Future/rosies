99961a833785cb8f5246535dd1f3200499209fe1
alvinzhengq <vortexdomainia@gmail.com>
2020-09-06 15:32:07 -0700

    Changed contact section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -8,6 +8,20 @@ export default class ContactSection extends Component {
                     <div className="container-fluid">
                         <div className="row justify-content-center mb-5">
                             <div className="col-md-10 text-center">
+                                <h2 className="mb-4 text-uppercase pb_letter-spacing-2">Hours</h2>
+                                <h3 className="mb-4">Monday - Friday: 9AM - 9PM</h3>
+                                <h3 className="mb-4">Saturday - Sunday: 10AM - 5PM</h3>
+                            </div>
+                        </div>
+
+                        <div className="row justify-content-center mb-5" style={{paddingTop: '3em'}}>
+                            <div className="col-md-10 text-center">
+                                <h3 className="mb-4">Phone Number: (888)-888-8888</h3>
+                                <h3 className="mb-4">Email Address: fakeemail@protonmail.com</h3>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center mb-5" style={{paddingTop: '3em'}}>
+                            <div className="col-md-10 text-center">
                                 <h2 className="mb-4 text-uppercase pb_letter-spacing-2">Instagram</h2>
                                 <button role="button" onClick={() => {window.open('https://www.instagram.com/liamwong/', '_blank')}} className="btn btn-ins btn-outline-primary" style={{cursor: 'pointer'}}><i className="fab fa-instagram pr-1"></i> Instagram</button>
                             </div>
@@ -26,21 +40,6 @@ export default class ContactSection extends Component {
                             <div className="card">
                                 <img className="card-img-top" src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/91083241_3132398746793685_6682983941027713226_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=38l9Cx7nqEoAX9tl4-K&oh=c7b4e32a7a377facdad0000a30d8ae82&oe=5F7E54C6" alt="Image caption here" />
                             </div>
-                            </div>
-                        </div>
-
-                        <div className="row justify-content-center mb-5" style={{paddingTop: '9em'}}>
-                            <div className="col-md-10 text-center">
-                                <h2 className="mb-4 text-uppercase pb_letter-spacing-2">Hours</h2>
-                                <h3 className="mb-4">Monday - Friday: 9AM - 9PM</h3>
-                                <h3 className="mb-4">Saturday - Sunday: 10AM - 5PM</h3>
-                            </div>
-                        </div>
-
-                        <div className="row justify-content-center mb-5" style={{paddingTop: '3em'}}>
-                            <div className="col-md-10 text-center">
-                                <h3 className="mb-4">Phone Number: (888)-888-8888</h3>
-                                <h3 className="mb-4">Email Address: fakeemail@protonmail.com</h3>
                             </div>
                         </div>
                     </div>
